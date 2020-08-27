2ab8294cb9fa8424882470d44c726c6396d5691a
Lucas Sousa <37560590+Lucas96Sousa@users.noreply.github.com>
2020-08-27 15:57:57 -0300

    Initial Commit

---
+++ src/pages/Main/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+import {Container, Label} from './styles';
+
+const Main = () => {
+  return (
+    <>
+      <Container>
+        <Label>Main</Label>
+      </Container>
+
+      <View>
+        <Text />
+      </View>
+    </>
+  );
+};
+
+export default Main;
---
+++ src/pages/Main/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  padding: 0px 10px;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Label = styled.Text`
+  font-size: 30px;
+`;
