ceaca4656928f1367440d43dc39be9b217004dde
Lucas Oliveira <lucas996oliveira@gmail.com>
2021-02-08 19:40:47 -0300

    ensure: NewEntry input mask

---
+++ src/pages/NewEntry/NewEntryInput/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+import {TextInputMask} from 'react-native-masked-text';
+
+const NewEnrtyInput = () => {
+  return (
+    <View>
+      <Text />
+    </View>
+  );
+};
+
+export default NewEnrtyInput;
